3dbeb25bcdd515acc8c1918202fc7a803062d92e
Your Name <you@example.com>
2021-02-16 13:30:50 +0000

    debug setting

--- pj_blog/settings.py
+++ pj_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&gu!13y)u^5buhjiw15602$g@ufj(eh6s6=b16td+5()gul!&7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
